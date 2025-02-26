25e8debf148802fb7c4b8db5432ec107ebaebecc
Manjeet Sharan <sharanmanjeet2004@gmail.com>
2025-02-26 10:57:55 +0530

    Fixed merge conflicts2

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: "/DynamicGraphs/", // 👈 Use your exact repository name!
 });
-=======
-  base: "/DynamicGraphs/", // 👈 Add your repo name here
-});
-
->>>>>>> 1251e32474618940c25b891b4ec3036a5a253d15
